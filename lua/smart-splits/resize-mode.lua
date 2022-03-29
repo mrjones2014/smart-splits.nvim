ffde3b5acc7c94467c2041e26eb68d92838bef8e
Mat Jones <mat@mjones.network>
2022-03-29 06:54:07 -0400

    make keymaps silent

--- lua/smart-splits/resize-mode.lua
+++ lua/smart-splits/resize-mode.lua
@@ -6,13 +6,20 @@ function M.start_resize_mode()
     return
   end
 
-  vim.api.nvim_set_keymap('n', 'h', ":lua require('smart-splits').resize_left()<CR>", {})
-  vim.api.nvim_set_keymap('n', 'l', ":lua require('smart-splits').resize_right()<CR>", {})
-  vim.api.nvim_set_keymap('n', 'j', ":lua require('smart-splits').resize_down()<CR>", {})
-  vim.api.nvim_set_keymap('n', 'k', ":lua require('smart-splits').resize_up()<CR>", {})
-  vim.api.nvim_set_keymap('n', '<ESC>', ":lua require('smart-splits.resize-mode').end_resize_mode()<CR>", {})
+  vim.api.nvim_set_keymap('n', 'h', ":lua require('smart-splits').resize_left()<CR>", { silent = true })
+  vim.api.nvim_set_keymap('n', 'l', ":lua require('smart-splits').resize_right()<CR>", { silent = true })
+  vim.api.nvim_set_keymap('n', 'j', ":lua require('smart-splits').resize_down()<CR>", { silent = true })
+  vim.api.nvim_set_keymap('n', 'k', ":lua require('smart-splits').resize_up()<CR>", { silent = true })
+  vim.api.nvim_set_keymap(
+    'n',
+    '<ESC>',
+    ":lua require('smart-splits.resize-mode').end_resize_mode()<CR>",
+    { silent = true }
+  )
 
-  vim.notify('Persistent resize mode enabled. Use h/j/k/l to resize, and <ESC> to finish.', vim.log.levels.INFO)
+  local msg = 'Persistent resize mode enabled. Use h/j/k/l to resize, and <ESC> to finish.'
+  print(msg)
+  vim.notify(msg, vim.log.levels.INFO)
 end
 
 function M.end_resize_mode()
@@ -22,7 +29,9 @@ function M.end_resize_mode()
   vim.api.nvim_del_keymap('n', 'k')
   vim.api.nvim_del_keymap('n', '<ESC>')
 
-  vim.notify('Persistent resize mode disabled. Normal keymaps have been restored.', vim.log.levels.INFO)
+  local msg = 'Persistent resize mode disabled. Normal keymaps have been restored.'
+  print(msg)
+  vim.notify(msg, vim.log.levels.INFO)
 end
 
 return M
