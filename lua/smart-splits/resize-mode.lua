4110cadc7ae912950fdac61a70d0a112e1404cf8
Mat Jones <mat@mjones.network>
2022-03-29 06:42:16 -0400

    add notifications

--- lua/smart-splits/resize-mode.lua
+++ lua/smart-splits/resize-mode.lua
@@ -11,6 +11,8 @@ function M.start_resize_mode()
   vim.api.nvim_set_keymap('n', 'j', ":lua require('smart-splits').resize_down()<CR>", {})
   vim.api.nvim_set_keymap('n', 'k', ":lua require('smart-splits').resize_up()<CR>", {})
   vim.api.nvim_set_keymap('n', '<ESC>', ":lua require('smart-splits.resize-mode').end_resize_mode()<CR>", {})
+
+  vim.notify('Persistent resize mode enabled. Use h/j/k/l to resize, and <ESC> to finish.', vim.log.levels.INFO)
 end
 
 function M.end_resize_mode()
@@ -19,6 +21,8 @@ function M.end_resize_mode()
   vim.api.nvim_del_keymap('n', 'j')
   vim.api.nvim_del_keymap('n', 'k')
   vim.api.nvim_del_keymap('n', '<ESC>')
+
+  vim.notify('Persistent resize mode disabled. Normal keymaps have been restored.', vim.log.levels.INFO)
 end
 
 return M
