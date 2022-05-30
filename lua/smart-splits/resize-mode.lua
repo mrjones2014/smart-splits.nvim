b83bd294b96e33d321f5be2a4e49465cb566e6cb
Mat Jones <mat@mjones.network>
2022-05-30 10:53:01 -0400

    add config options for persistent resize mode
    fixes #18

--- lua/smart-splits/resize-mode.lua
+++ lua/smart-splits/resize-mode.lua
@@ -6,28 +6,38 @@ function M.start_resize_mode()
     return
   end
 
+  local quit_key = require('smart-splits.config').resize_mode_quit_key
   vim.api.nvim_set_keymap('n', 'h', ":lua require('smart-splits').resize_left()<CR>", { silent = true })
   vim.api.nvim_set_keymap('n', 'l', ":lua require('smart-splits').resize_right()<CR>", { silent = true })
   vim.api.nvim_set_keymap('n', 'j', ":lua require('smart-splits').resize_down()<CR>", { silent = true })
   vim.api.nvim_set_keymap('n', 'k', ":lua require('smart-splits').resize_up()<CR>", { silent = true })
   vim.api.nvim_set_keymap(
     'n',
-    '<ESC>',
+    quit_key,
     ":lua require('smart-splits.resize-mode').end_resize_mode()<CR>",
     { silent = true }
   )
 
-  local msg = 'Persistent resize mode enabled. Use h/j/k/l to resize, and <ESC> to finish.'
+  if require('smart-splits.config').resize_mode_silent then
+    return
+  end
+
+  local msg = string.format('Persistent resize mode enabled. Use h/j/k/l to resize, and %s to finish.', quit_key)
   print(msg)
   vim.notify(msg, vim.log.levels.INFO)
 end
 
 function M.end_resize_mode()
+  local quit_key = require('smart-splits.config').resize_mode_quit_key
   vim.api.nvim_del_keymap('n', 'h')
   vim.api.nvim_del_keymap('n', 'l')
   vim.api.nvim_del_keymap('n', 'j')
   vim.api.nvim_del_keymap('n', 'k')
-  vim.api.nvim_del_keymap('n', '<ESC>')
+  vim.api.nvim_del_keymap('n', quit_key)
+
+  if require('smart-splits.config').resize_mode_silent then
+    return
+  end
 
   local msg = 'Persistent resize mode disabled. Normal keymaps have been restored.'
   print(msg)
