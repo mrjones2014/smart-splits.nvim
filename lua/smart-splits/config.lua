0cbeb9b5330489481a6c33c3239cc47f99d384eb
L2AsHdz <leonidas0197@gmail.com>
2022-06-15 12:40:56 -0600

    Add hooks

--- lua/smart-splits/config.lua
+++ lua/smart-splits/config.lua
@@ -8,24 +8,18 @@ local M = {
     'NvimTree',
   },
   move_cursor_same_row = false,
-  resize_mode_quit_key = '<ESC>',
-  resize_mode_silent = false,
+  resize_mode = {
+    quit_key = '<ESC>',
+    silent = false,
+    hooks = {
+      on_enter = nil,
+      on_leave = nil,
+    },
+  },
 }
 
-local function default_bool(value, default)
-  if value == nil then
-    return default
-  else
-    return value
-  end
-end
-
 function M.setup(config)
-  M.ignored_buftypes = config.ignored_buftypes or M.ignored_buftypes
-  M.ignored_filetypes = config.ignored_filetypes or M.ignored_filetypes
-  M.move_cursor_same_row = default_bool(config.move_cursor_same_row, M.move_cursor_same_row)
-  M.resize_mode_quit_key = config.resize_mode_quit_key or M.resize_mode_quit_key
-  M.resize_mode_silent = default_bool(config.resize_mode_silent, M.resize_mode_silent)
+  M = vim.tbl_deep_extend('force', M, config)
 end
 
 return M
