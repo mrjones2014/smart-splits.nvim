957ff9051a1c31a499f29b5b10019747a7dcccc1
L2AsHdz <leonidas0197@gmail.com>
2022-06-16 21:26:02 -0600

    fix: resize_mode table is not defined

--- lua/smart-splits/config.lua
+++ lua/smart-splits/config.lua
@@ -25,14 +25,32 @@ local function default_bool(value, default)
   return value
 end
 
+local function default_hooks(new_config)
+  if not new_config then
+    return M.resize_mode.hooks
+  end
+  return {
+    on_enter = new_config.on_enter or M.resize_mode.hooks.on_enter,
+    on_leave = new_config.on_leave or M.resize_mode.hooks.on_leave,
+  }
+end
+
+local function default_resize_mode(new_config)
+  if not new_config then
+    return M.resize_mode
+  end
+  return {
+    quit_key = new_config.quit_key or M.resize_mode.quit_key,
+    silent = default_bool(new_config.silent, M.resize_mode.silent),
+    hooks = default_hooks(new_config.hooks),
+  }
+end
+
 function M.setup(config)
   M.ignored_buftypes = config.ignored_buftypes or M.ignored_buftypes
   M.ignored_filetypes = config.ignored_filetypes or M.ignored_filetypes
   M.move_cursor_same_row = default_bool(config.move_cursor_same_row, M.move_cursor_same_row)
-  M.resize_mode.quit_key = config.resize_mode.quit_key or M.resize_mode.quit_key
-  M.resize_mode.silent = default_bool(config.resize_mode.silent, M.resize_mode.silent)
-  M.resize_mode.hooks.on_enter = config.resize_mode.hooks.on_enter or M.resize_mode.hooks.on_enter
-  M.resize_mode.hooks.on_leave = config.resize_mode.hooks.on_leave or M.resize_mode.hooks.on_leave
+  M.resize_mode = default_resize_mode(config.resize_mode)
 
   -- TODO: Remove this code block in the next commits
   if config.resize_mode_quit_key then
