9a5a23fce3f66c5bfae6553f6bc6079d047b72e2
Mat Jones <mat@mjones.network>
2023-04-05 10:38:47 -0400

    use new wezterm cli function if available

--- lua/smart-splits/mux/wezterm.lua
+++ lua/smart-splits/mux/wezterm.lua
@@ -31,6 +31,12 @@ function M.current_pane_id()
 end
 
 function M.current_pane_at_edge(direction)
+  -- try the new way first
+  local output = wezterm_exec({ 'get-pane-direction', direction })
+  if vim.v.shell_error == 0 then
+    local ok, value = pcall(tonumber, output)
+    return ok and value ~= nil
+  end
   local pane_id = M.current_pane_id()
   wezterm_exec({ 'activate-pane-direction', direction })
   local new_pane_id = M.current_pane_id()
