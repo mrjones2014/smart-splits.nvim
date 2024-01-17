ec9a2210433ebd51bde7f695a4f2ec02d1ac95c1
nmc <mm.mochou+github@gmail.com>
2024-01-17 20:13:11 +0800

    kitty.conf remove regular match

--- kitty/pass_keys.py
+++ kitty/pass_keys.py
@@ -1,14 +1,8 @@
-import re
-
 from kittens.tui.handler import result_handler
+
 from kitty.key_encoding import KeyEvent, parse_shortcut
 
-
-def is_window_vim(window, vim_id):
-    from kittens.tui.loop import debug
-    debug(vim_id)
-    fp = window.child.foreground_processes
-    return any(re.search(vim_id, p['cmdline'][0] if len(p['cmdline']) else '', re.I) for p in fp)
+# from kitty.keys import keyboard_mode_name
 
 
 def encode_key_mapping(window, key_mapping):
@@ -26,58 +20,61 @@ def encode_key_mapping(window, key_mapping):
 
     return window.encoded_key(event)
 
+
 def relative_resize_window(direction, amount, target_window_id, boss):
     window = boss.window_id_map.get(target_window_id)
     if window is None:
         return
 
-    neighbors = boss.active_tab.current_layout.neighbors_for_window(window, boss.active_tab.windows)
+    neighbors = boss.active_tab.current_layout.neighbors_for_window(
+        window, boss.active_tab.windows
+    )
     current_window_id = boss.active_tab.active_window
 
-    left_neighbors = neighbors.get('left')
-    right_neighbors = neighbors.get('right')
-    top_neighbors = neighbors.get('top')
-    bottom_neighbors = neighbors.get('bottom')
+    left_neighbors = neighbors.get("left")
+    right_neighbors = neighbors.get("right")
+    top_neighbors = neighbors.get("top")
+    bottom_neighbors = neighbors.get("bottom")
 
     # has a neighbor on both sides
-    if direction == 'left' and (left_neighbors and right_neighbors):
-        boss.active_tab.resize_window('narrower', amount)
+    if direction == "left" and (left_neighbors and right_neighbors):
+        boss.active_tab.resize_window("narrower", amount)
     # only has left neighbor
-    elif direction == 'left' and left_neighbors:
-        boss.active_tab.resize_window('wider', amount)
+    elif direction == "left" and left_neighbors:
+        boss.active_tab.resize_window("wider", amount)
     # only has right neighbor
-    elif direction == 'left' and right_neighbors:
-        boss.active_tab.resize_window('narrower', amount)
+    elif direction == "left" and right_neighbors:
+        boss.active_tab.resize_window("narrower", amount)
 
     # has a neighbor on both sides
-    elif direction == 'right' and (left_neighbors and right_neighbors):
-        boss.active_tab.resize_window('wider', amount)
+    elif direction == "right" and (left_neighbors and right_neighbors):
+        boss.active_tab.resize_window("wider", amount)
     # only has left neighbor
-    elif direction == 'right' and left_neighbors:
-        boss.active_tab.resize_window('narrower', amount)
+    elif direction == "right" and left_neighbors:
+        boss.active_tab.resize_window("narrower", amount)
     # only has right neighbor
-    elif direction == 'right' and right_neighbors:
-        boss.active_tab.resize_window('wider', amount)
+    elif direction == "right" and right_neighbors:
+        boss.active_tab.resize_window("wider", amount)
 
     # has a neighbor above and below
-    elif direction == 'up' and (top_neighbors and bottom_neighbors):
-        boss.active_tab.resize_window('shorter', amount)
+    elif direction == "up" and (top_neighbors and bottom_neighbors):
+        boss.active_tab.resize_window("shorter", amount)
     # only has top neighbor
-    elif direction == 'up' and top_neighbors:
-        boss.active_tab.resize_window('taller', amount)
+    elif direction == "up" and top_neighbors:
+        boss.active_tab.resize_window("taller", amount)
     # only has bottom neighbor
-    elif direction == 'up' and bottom_neighbors:
-        boss.active_tab.resize_window('shorter', amount)
+    elif direction == "up" and bottom_neighbors:
+        boss.active_tab.resize_window("shorter", amount)
 
     # has a neighbor above and below
-    elif direction == 'down' and (top_neighbors and bottom_neighbors):
-        boss.active_tab.resize_window('taller', amount)
+    elif direction == "down" and (top_neighbors and bottom_neighbors):
+        boss.active_tab.resize_window("taller", amount)
     # only has top neighbor
-    elif direction == 'down' and top_neighbors:
-        boss.active_tab.resize_window('shorter', amount)
+    elif direction == "down" and top_neighbors:
+        boss.active_tab.resize_window("shorter", amount)
     # only has bottom neighbor
-    elif direction == 'down' and bottom_neighbors:
-        boss.active_tab.resize_window('taller', amount)
+    elif direction == "down" and bottom_neighbors:
+        boss.active_tab.resize_window("taller", amount)
 
 
 def main():
@@ -89,18 +86,21 @@ def handle_result(args, result, target_window_id, boss):
     window = boss.window_id_map.get(target_window_id)
     action = args[1]
     direction = args[2]
-    key_mapping = args[3] if action == 'neighboring_window' else args[4]
-    amount = int(args[3]) if action == 'relative_resize' else None
-    vim_id_idx = 4 if action == 'neighboring_window' else 5
-    vim_id = args[vim_id_idx] if len(args) > vim_id_idx else "n?vim"
+    key_mapping = args[3] if action == "neighboring_window" else args[4]
+    amount = int(args[3]) if action == "relative_resize" else None
 
     if window is None:
         return
-    if is_window_vim(window, vim_id):
+
+    cmd = window.child.foreground_cmdline[0]
+
+    # keyboard_mode_name(window.screen) == "kitty"  You can get kitty.
+    # result: zsh, tmux, nvim
+    if cmd[-4:] == "nvim":
         for keymap in key_mapping.split(">"):
             encoded = encode_key_mapping(window, keymap)
             window.write_to_child(encoded)
-    elif action == 'neighboring_window':
+    elif action == "neighboring_window":
         boss.active_tab.neighboring_window(direction)
-    elif action == 'relative_resize':
+    elif action == "relative_resize":
         relative_resize_window(direction, amount, target_window_id, boss)
