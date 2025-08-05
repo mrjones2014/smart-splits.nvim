8af4770f23e481f44e96581d3cb89330090aa144
Mat Jones <mat@mjones.network>
2025-08-05 09:50:19 -0400

    fix(mux): Remove unneeded Kitty kitten for performance

--- kitty/install-kittens.bash
+++ kitty/install-kittens.bash
@@ -2,6 +2,5 @@
 
 PREFIX="${XDG_CONFIG_HOME:-$HOME/.config}"
 KITTY_CONFIG_PATH="$PREFIX/kitty"
-cp -f ./kitty/neighboring_window.py "$KITTY_CONFIG_PATH/"
 cp -f ./kitty/relative_resize.py "$KITTY_CONFIG_PATH/"
 cp -f ./kitty/split_window.py "$KITTY_CONFIG_PATH/"
