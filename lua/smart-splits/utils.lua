c4afaf23141651845e6e1966d936d79ff8939e4d
Mat Jones <mat@mjones.network>
2025-10-06 09:06:14 -0400

    fix: add special handling for Snacks.explorer window

--- lua/smart-splits/utils.lua
+++ lua/smart-splits/utils.lua
@@ -10,13 +10,30 @@ function M.tbl_find(tbl, predicate)
   return nil
 end
 
+---Check for some special cases like Snacks.explorer(),
+---see https://github.com/mrjones2014/smart-splits.nvim/issues/342
+local function is_special_floating_window_treat_as_non_floating(win_id, win_config)
+  win_id = win_id or 0
+  local buf = vim.api.nvim_win_get_buf(win_id)
+  local ft = vim.bo[buf].filetype
+  -- the snacks explorer is just a snacks picker in disguise;
+  -- it is technically a floating window, but on the same z index
+  -- as the main window, so we want to treat it as a normal window
+  if ft == 'snacks_picker_list' and win_config.zindex == 33 then
+    return true
+  end
+end
+
 ---Check if a window is a floating window
 ---@param win_id number|nil window ID to check, defaults to current window (0)
 ---@return boolean
 function M.is_floating_window(win_id)
   win_id = win_id or 0
   local win_cfg = vim.api.nvim_win_get_config(win_id)
-  return win_cfg and (win_cfg.relative ~= '' or not win_cfg.relative)
+  if is_special_floating_window_treat_as_non_floating(win_id, win_cfg) then
+    return false
+  end
+  return win_cfg.relative ~= ''
 end
 
 return M
