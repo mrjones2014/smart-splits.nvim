2d9edfaaf8cf13dada79c28ad2ab6880969bb415
Micael Viana <44692237+micaelviana@users.noreply.github.com.>
2026-06-15 09:57:31 -0400

    fix: use explicit current-directory paths in kitty integration script

--- kitty/install-kittens.bash
+++ kitty/install-kittens.bash
@@ -2,6 +2,6 @@
 
 PREFIX="${XDG_CONFIG_HOME:-$HOME/.config}"
 KITTY_CONFIG_PATH="$PREFIX/kitty"
-cp -f neighboring_window.py "$KITTY_CONFIG_PATH/"
-cp -f relative_resize.py "$KITTY_CONFIG_PATH/"
-cp -f split_window.py "$KITTY_CONFIG_PATH/"
+cp -f ./neighboring_window.py "$KITTY_CONFIG_PATH/"
+cp -f ./relative_resize.py "$KITTY_CONFIG_PATH/"
+cp -f ./split_window.py "$KITTY_CONFIG_PATH/"
