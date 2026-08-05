5de4ea1d4e88b7bb8d33e012d40fb8634c3d3bb5
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2026-08-06 00:08:18 +0200

    fix(zellij): Improve stability and performance for zellij
    - Fixed broken navigation when `at_edge="stop"`
    - Navigation is now instantaneous
    - Remove support for `at_edge="wrap"` and `at_edge="split"`

--- lua/smart-splits/mux/zellij.lua
+++ lua/smart-splits/mux/zellij.lua
@@ -14,44 +14,17 @@ local M = {} ---@diagnostic disable-line: missing-fields
 
 M.type = 'zellij'
 
+-- It is very expensive to ask zellij for the current pane id.
+-- We also don't need the ID to move to the next pane.
+-- However, SmartSplitsMultiplexer (and ./init.lua) demands that we return a number.
+-- So let's just return -1
 function M.current_pane_id()
-  local output = vim.split(zellij_exec({ 'action', 'list-clients' }), '\n', { trimempty = true })
-  if not output[2] then
-    return nil
-  end
-
-  -- The output format is like
-  -- ```
-  -- CLIENT_ID ZELLIJ_PANE_ID RUNNING_COMMAND
-  -- 1         terminal_0     /path/to/nvim --cmd lua print('some arguments')
-  -- ```
-  -- We are looking for the value `0` here in the `terminal_0` chunk.
-  -- The `terminal_` prefix might be something else, for example if a plugin's UI
-  -- is currently focused, but we still need to know the pane ID, so we're using the
-  -- `%w+` pattern to match any word prefix. Then we capture the ID with the `%d` pattern
-  -- in the capture group.
-  local pane_id = string.match(output[2], '%S+%s+%w+_(%d+)')
-  return pane_id
+  return -1
 end
 
+-- Not supported yet: https://github.com/mrjones2014/smart-splits.nvim/issues/477
 function M.current_pane_at_edge()
-  local pane_id = M.current_pane_id()
-  if pane_id == nil then
-    log.warn('could not get zeillij pane id')
-    return false
-  end
-  zellij_exec({ 'action', 'move-focus', Direction.left })
-  local new_pane_id = M.current_pane_id()
-
-  if new_pane_id == nil then
-    log.warn('could not get zeillij pane id')
-    return false
-  end
-
-  -- move back to original pane
-  zellij_exec({ 'action', 'move-focus', Direction.right })
-
-  return pane_id == new_pane_id
+  return false
 end
 
 -- amount is not supported on zellij
@@ -88,30 +61,34 @@ function M.next_pane(direction)
   return code == 0
 end
 
--- size is not supported on zellij
 function M.split_pane(direction, _size) ---@diagnostic disable-line: unused-local
-  -- zellij only splits right and down; for the others,
-  -- we must split right and down then swap the panes
-  local args = { 'action', 'new-pane' }
-  local need_swap
-  if direction == Direction.left then
-    table.insert(args, 'right')
-    need_swap = 'right'
-  elseif direction == Direction.up then
-    table.insert(args, 'down')
-    need_swap = 'down'
-  else
-    table.insert(args, direction)
-  end
-  local _, split_code = zellij_exec(args)
-  if need_swap ~= nil then
-    local _, swap_code = zellij_exec({ 'action', 'move-pane', need_swap })
-    M.update_mux_layout_details()
-    return split_code == 0 and swap_code == 0
-  end
-  M.update_mux_layout_details()
-  return split_code == 0
+  return false
 end
+
+-- size is not supported on zellij
+-- function M.split_pane(direction, _size) ---@diagnostic disable-line: unused-local
+--   -- zellij only splits right and down; for the others,
+--   -- we must split right and down then swap the panes
+--   local args = { 'action', 'new-pane' }
+--   local need_swap
+--   if direction == Direction.left then
+--     table.insert(args, 'right')
+--     need_swap = 'right'
+--   elseif direction == Direction.up then
+--     table.insert(args, 'down')
+--     need_swap = 'down'
+--   else
+--     table.insert(args, direction)
+--   end
+--   local _, split_code = zellij_exec(args)
+--   if need_swap ~= nil then
+--     local _, swap_code = zellij_exec({ 'action', 'move-pane', need_swap })
+--     M.update_mux_layout_details()
+--     return split_code == 0 and swap_code == 0
+--   end
+--   M.update_mux_layout_details()
+--   return split_code == 0
+-- end
 
 function M.update_mux_layout_details()
   -- Not implemented yet - check Kitty mux for reference
