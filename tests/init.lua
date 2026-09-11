fb59f6058eb1485ad9c2dae5daaa5e8cf0ce1d86
Mat Jones <mat@mjones.network>
2026-09-11 12:08:38 -0400

    fix(test): Silence logs in tests

--- tests/init.lua
+++ tests/init.lua
@@ -4,3 +4,10 @@ vim.opt.rtp:append('.')
 -- Disable swap files and shada for test runs
 vim.o.swapfile = false
 vim.o.shadafile = 'NONE'
+
+-- Silence echo/notify during tests so intentional error paths don't pollute output
+vim.api.nvim_echo = function() end ---@diagnostic disable-line: duplicate-set-field
+vim.notify = function() end ---@diagnostic disable-line: duplicate-set-field
+
+-- do not write to log file in tests
+require('smart-splits.config').log.file = false
